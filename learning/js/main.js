9fc0cd1c9b54b8f358e3139952d208f6fd3b95da
xiebaochun <463449055@qq.com>
2014-07-03 16:25:36 +0800

    update

--- learning/js/main.js
+++ learning/js/main.js
@@ -1,18 +1,19 @@
 $(document).ready(function(){
 
-	    console.log("enter jquery ready");
+	    //console.log("enter jquery ready");
         $("#left_nav li").each(function(){
 
         	$(this).hover(
         		function(){
-        			// $("#left_nav li").each(function(){
-        			//   $(this).removeClass("left_border_red");	
-        			// });
-        	            console.log("enter hover");
+        			$("#left_nav li").each(function(){
+        			  $(this).removeClass("left_border_red");	
+        			});
+        	            //console.log("enter hover");
 	                   $(this).addClass("left_border_red");
 	        		},function(){
-	        			console.log("out hover");
-	                    $(this).removeClass("left_border_red");
+	        			//console.log("out hover");
+	                   $(this).removeClass("left_border_red");
+	                   $("#nav_01").addClass("left_border_red");
 	        		}
         	);
         });
