ee0f98b071036a664eb063f909844a858bb8e7bf
xiebaochun <463449055@qq.com>
2014-07-03 16:05:05 +0800

    update

---
+++ learning/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(
+        $("#left_nav li").each(function(){
+
+        	$(this).hover(
+        		function(){
+        			$("#left_nav li").each(function(){
+        			  $(this).removeClass("left_border_red");	
+        			});
+                   $(this).addClass("left_border_red");
+        		},function(){
+                    $(this).removeClass("left_border_red");
+        		}
+        		);
+        });
+
+
+	);
