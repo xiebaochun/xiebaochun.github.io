83dddaebf4636506333cf04a6706a76f30561ece
xiebaochun <463449055@qq.com>
2014-07-03 16:09:02 +0800

    update

--- learning/js/main.js
+++ learning/js/main.js
@@ -3,14 +3,14 @@ $(document).ready(
 
         	$(this).hover(
         		function(){
-        			$("#left_nav li").each(function(){
-        			  $(this).removeClass("left_border_red");	
-        			});
-                   $(this).addClass("left_border_red");
-        		},function(){
-                    $(this).removeClass("left_border_red");
-        		}
-        		);
+        			// $("#left_nav li").each(function(){
+        			//   $(this).removeClass("left_border_red");	
+        			// });
+	                   $(this).addClass("left_border_red");
+	        		},function(){
+	                    $(this).removeClass("left_border_red");
+	        		}
+        	);
         });
 
 
