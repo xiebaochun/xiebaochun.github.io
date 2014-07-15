82dfc89b550e397798e26aec7b9b6d825dd865aa
xiebaochun <463449055@qq.com>
2014-07-15 14:57:29 +0800

    update

--- learning/js/main.js
+++ learning/js/main.js
@@ -6,17 +6,47 @@ $(document).ready(function(){
         	$(this).hover(
         		function(){
 	        			$("#left_nav li").each(function(){
-	        			  $(this).removeClass("left_border_red");	
+	        			  $(this).removeClass("left_border_red");
+	        			  	$(this).removeClass("right_border_gray");
 	        			});
         	            //console.log("enter hover");
 	                   $(this).addClass("left_border_red");
+	                   $(this).addClass("right_border_gray");
+
 	        		},function(){
 	        			//console.log("out hover");
-	                   $(this).removeClass("left_border_red");
-	                   $("#nav_01").addClass("left_border_red");
+	                   // $(this).removeClass("left_border_red");
+	                   // $("#nav_01").addClass("left_border_red");
 	        		}
         	);
         });
+
+        getContent();
+        function getContent(){
+        	var xmlhttp;
+        	if(window.XMLHttpRequest){
+        		xmlhttp=new XMLHttpRequest();
+        		console.log("enable the XMLHttpRequest")
+        	}else{
+        		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+        		console.log("enable the ActiveXObject")
+        	}
+        	xmlhttp.open("GET","test.text",true);
+        	xmlhttp.send();
+        	console.log("xmlhttp readyState:"+xmlhttp.readyState);
+        	console.log(xmlhttp);
+        	xmlhttp.onreadystatechange=function()
+        	{
+        		if(xmlhttp.readyState==4&&xmlhttp.status==200)
+        		{
+        	         console.log(xmlhttp.response+"serger");	
+        	         //$(".right-content").	
+        	         document.getElementById("right").innerHTML=xmlhttp.responseText;
+        		}
+        	}
+        	//var responseText=xmlhttp.responseText;
+        	
+        }
 
 
 	});
