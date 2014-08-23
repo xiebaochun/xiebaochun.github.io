bbd258f66517dd54fc351c148557a09632052e45
xiebaochun <463449055@qq.com>
2014-08-23 09:59:49 +0800

    update

--- common/common.js
+++ common/common.js
@@ -1,6 +1,8 @@
  $(document).ready(function(){
 	$.get("http://xiebaochun.github.io/common/footer.html",function(data){
-          console.log(data);
+          //console.log(data);
+        //add footer.html to the footer dom
+        $("#foot_wrap").html(data);
 	}).fail(function(){
 		console.log("get footer.html fail!");
 	});
