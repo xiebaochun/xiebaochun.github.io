fa9aa7db073ada71926300324fc1ee493ee5d206
xiebaochun <463449055@qq.com>
2014-08-23 10:19:26 +0800

    update

--- common/common.js
+++ common/common.js
@@ -6,4 +6,11 @@
 	}).fail(function(){
 		console.log("get footer.html fail!");
 	});
+	$.get("http://xiebaochun.github.io/common/header.html",function(data){
+          //console.log(data);
+        //add footer.html to the footer dom
+        $("#header").html(data);
+	}).fail(function(){
+		console.log("get header.html fail!");
+	});
 });
