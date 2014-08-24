ee7b26285e7759501863bc2c868f117b1bb9c887
xiebaochun <463449055@qq.com>
2014-08-24 21:04:49 +0800

    update

--- HTML5/js/main.js
+++ HTML5/js/main.js
@@ -43,7 +43,16 @@ $(document).ready(function(){
                 //      // $(".primary_nav").css({"position":"relative","top":"5px"});
                 // }  
                 
-            });  
+            }); 
+
+    $(".product_item_title").hover(function(){
+        $(this).css("background","#fff5f5");
+     },function(){
+        if($(this).parent().height()<32){
+            $(this).css("background","#f5f5f5");
+        }     
+     });
+
     $(".product_item_title").click(function(){
         var self=this;
         // if($(this).parent().height()<300){
@@ -57,31 +66,33 @@ $(document).ready(function(){
             
         //     });
         // }
-        if($(this).parent().height()<300){
+        var height=$(this).parent().height();
+        // console.log(height);
+        if(height<32){
            
             //forech all product_item,if which height large than 31px then scale down it
             $("#front-end .product_item").each(function(){
-                console.log(this);
+                // console.log(this);
                 if($(this).height()>31){
                    $(this).animate({height:"31px"},200,function(){
 
                         $(this).children(".product_item_title").children(".product_item_title_right").html(">");
-                        $(this).children(".product_item_title").css({"border-bottom":"1px solid #dedede","color":"#aaa"});
+                        $(this).children(".product_item_title").css({"border-bottom":"1px solid #dedede","color":"#aaa","background": "#f5f5f5"});
 
                     });        
                 }
             });
             //if product_item,which was clicked,height less than 300px then scale up it
             $(self).children(".product_item_title_right").html("V");
-                        $(self).css({"border-bottom":"1px solid #fedede","color":"#777"});
+                        $(self).css({"border-bottom":"1px solid #fedede","color":"#777","background": "#fff5f5"});
                          $(self).parent().animate({height:"300px"},500,function(){
                                
                          });
             //if large than 299px,then scale down it
-        }else{
+        }else if(height>299){
             $(self).children(".product_item_title_right").html(">");   
             $(self).parent().animate({height:"31px"},200,function(){
-                       $(self).css({"border-bottom":"1px solid #dedede","color":"#aaa"});
+                       $(self).css({"border-bottom":"1px solid #dedede","color":"#aaa","background": "#f5f5f5"});
                  });
             }
 
