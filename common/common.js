1e91819c79a4adae0b1fa29073873fb752a25440
xiebaochun <463449055@qq.com>
2014-09-02 09:48:48 +0800

    update

--- common/common.js
+++ common/common.js
@@ -1,14 +1,14 @@
  $(document).ready(function(){
- 	var host = window.location.hostname;
- 	console.log(host);
-	$.get(host+"/common/footer.html",function(data){
+ 	var hostName = window.location.hostname;
+ 	//console.log(hostName);
+	$.get("http://"+hostName+"/common/footer.html",function(data){
           //console.log(data);
         //add footer.html to the footer dom
         $("#foot_wrap").html(data);
 	}).fail(function(){
 		console.log("get footer.html fail!");
 	});
-	$.get(host+"/common/header.html",function(data){
+	$.get("http://"+hostName+"/common/header.html",function(data){
           //console.log(data);
         //add footer.html to the footer dom
         $("#header").html(data);
