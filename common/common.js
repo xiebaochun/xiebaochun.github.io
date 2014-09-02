1f1fc8db11d13f73a12277924e010b5d9b1ba38a
xiebaochun <463449055@qq.com>
2014-09-02 09:43:05 +0800

    update

--- common/common.js
+++ common/common.js
@@ -1,12 +1,14 @@
  $(document).ready(function(){
-	$.get("http://xiebaochun.github.io/common/footer.html",function(data){
+ 	var host = window.location.hostname;
+ 	console.log(host);
+	$.get(host+"/common/footer.html",function(data){
           //console.log(data);
         //add footer.html to the footer dom
         $("#foot_wrap").html(data);
 	}).fail(function(){
 		console.log("get footer.html fail!");
 	});
-	$.get("http://xiebaochun.github.io/common/header.html",function(data){
+	$.get(host+"/common/header.html",function(data){
           //console.log(data);
         //add footer.html to the footer dom
         $("#header").html(data);
